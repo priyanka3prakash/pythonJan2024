429e10aa7e9a87961ce40fa8f4e94104db1d3010
Priyanka <130571847+priyanka3prakash@users.noreply.github.com>
2024-02-26 22:35:39 +0000

    class21

---
+++ 09_Iterators_generators_coroutines/01_Iterables/b_enumerate_function.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Purpose: Iterating and using enumerate()
+
+"""
+my_string = "Program"
+
+count = 0
+for ech_chr in my_string:
+    print(count, ech_chr)
+    count += 1
+
+print()
+for ech_chr in enumerate(my_string):
+    print(ech_chr)
+
+print()
+for loop_index, ech_chr in enumerate(my_string):
+    print(f"At loop {loop_index:2}, we found {ech_chr}")
+
+print()
+for loop_index, ech_chr in enumerate(my_string, start=0):
+    print(f"At loop {loop_index:2}, we found {ech_chr}")
+
+print()
+for loop_index, ech_chr in enumerate(my_string, start=4):
+    print(f"At loop {loop_index:2}, we found {ech_chr}")
+
+print()
+for loop_index, ech_chr in enumerate(my_string, start=-3):
+    print(f"At loop {loop_index:2}, we found {ech_chr}")
+
+# for loop_index, ech_chr in enumerate(my_string, start=-3.5):
+#     print(f'At loop {loop_index:2}, we found {ech_chr}')
+# TypeError: 'float' object cannot be interpreted as an integer
