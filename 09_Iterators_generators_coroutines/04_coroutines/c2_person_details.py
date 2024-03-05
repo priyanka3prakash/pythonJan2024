3603014a18c34c1418d207479ffebc70f6a7c847
Priyanka <130571847+priyanka3prakash@users.noreply.github.com>
2024-03-05 23:58:51 +0000

    class22

---
+++ 09_Iterators_generators_coroutines/04_coroutines/c2_person_details.py
@@ -0,0 +1,16 @@
+
+
+def person_details():
+    while True:
+        yield 
+
+        print(f''' 
+                Name: {name}
+                Age : {age}
+                Salary: {salary}
+
+            ''')
+
+
+
+name, age, salary
